aba1023946113c077841d453a3ddd2a17a8a39c8
Eric Buchlin <eric.buchlin@ias.u-psud.fr>
2023-09-08 15:18:24 +0200

    Use requests payload to do queries

--- soda/availability.py
+++ soda/availability.py
@@ -1,14 +1,79 @@
-from datetime import datetime, date
-import pathlib
+from pathlib import Path
 import pandas as pd
-
 import requests
-from sunpy import time
 
-_MISSION_BEGIN = time.parse_time(datetime(2020, 1, 1))
-_NOW = time.parse_time(datetime.now())
-_CACHE_DIR = (pathlib.Path(__file__) / '..' / '.data').resolve()
+_MISSION_BEGIN = pd.Timestamp("2020-01-01")
+_NOW = pd.Timestamp("now")
+_CACHE_DIR = (Path(__file__) / '..' / '.data').resolve()
 _CACHE_DIR.mkdir(exist_ok=True)
+
+
+class SoarQuery:
+    tap_end_point = 'http://soar.esac.esa.int/soar-sl-tap/tap/'
+
+    def __init__(self):
+        pass
+
+    def _soar_sync_query(self, adql_query):
+        """
+        SOAR synchronous TAP query
+
+        Parameters
+        ----------
+        adql_query: str
+            ADQL query
+
+        Return
+        ------
+        dict
+            JSON query result
+        """
+        payload = {
+            'REQUEST': 'doQuery',
+            'LANG': 'ADQL',
+            'FORMAT': 'json',
+            'QUERY': adql_query
+        }
+        r = requests.get(f'{self.tap_end_point}/sync', params=payload)
+        return r.json()
+
+    def get_descriptors(self, instruments):
+        """
+        Get a list of file descriptors for the given instruments
+        """
+        query = "SELECT DISTINCT descriptor FROM soar.v_sc_data_item"
+        result = self._soar_sync_query(query)
+        descriptors = [descriptor[0] for descriptor in result["data"]]
+        return sorted(descriptors)
+
+    def get_availability(self, descriptor, begin_time, end_time, low_latency=False):
+        """
+        Get data availability from SOAR for the given descriptor and time range
+
+        Parameters
+        ----------
+        descriptor: str
+            Data product descriptor
+        begin_time: pandas.Timestamp
+            Start of time range
+        end_time: pandas.Timestamp
+            End of time range
+        low_latency: bool
+            If True, select low latency data
+        """
+        query = {}
+        query['SELECT'] = '*'
+        if low_latency:
+            query['FROM'] = 'v_ll_data_item'
+        else:
+            query['FROM'] = 'v_sc_data_item'
+        query['WHERE'] = (f"descriptor='{descriptor}' AND "
+                          f"begin_time<='{end_time}' AND "
+                          f"begin_time>='{begin_time}'")
+        adql = ' '.join([f'{item} {query[item]}' for
+                                          item in query])
+        result = self._soar_sync_query(adql)
+        return result
 
 
 class DataProduct:
@@ -52,48 +117,27 @@ class DataProduct:
         Solar Orbiter Archive for a given data descriptor.
         """
         print(f'Updating intervals for {self.descriptor}...')
-        base_url = ('http://soar.esac.esa.int/soar-sl-tap/tap/'
-                    'sync?REQUEST=doQuery&')
-        begin_time = _MISSION_BEGIN.isot.replace('T', '+')
-        end_time = _NOW.isot.replace('T', '+')
-        # Need to manually set the intervals based on a query
-        request_dict = {}
-        request_dict['LANG'] = 'ADQL'
-        request_dict['FORMAT'] = 'json'
-
-        query = {}
-        query['SELECT'] = '*'
-        if self.low_latency:
-            query['FROM'] = 'v_ll_data_item'
-        else:
-            query['FROM'] = 'v_sc_data_item'
-        query['WHERE'] = (f"descriptor='{self.descriptor}'+AND+"
-                          f"begin_time<='{end_time}'+AND+"
-                          f"begin_time>='{begin_time}'")
-        request_dict['QUERY'] = '+'.join([f'{item}+{query[item]}' for
-                                          item in query])
-
-        request_str = ''
-        request_str = [f'{item}={request_dict[item]}' for item in request_dict]
-        request_str = '&'.join(request_str)
-
-        url = base_url + request_str
-        # Get request info
-        r = requests.get(url)
-        # TODO: intelligently detect and error on a bad descriptor
+        begin_time = _MISSION_BEGIN.isoformat(sep=" ")
+        end_time = _NOW.isoformat(sep=" ")
+        soar = SoarQuery()
+        result = soar.get_availability(
+            self.descriptor,
+            begin_time, end_time,
+            low_latency=self.low_latency
+        )
 
         # Do some list/dict wrangling
-        names = [m['name'] for m in r.json()['metadata']]
+        names = [m['name'] for m in result['metadata']]
         info = {name: [] for name in names}
-        for entry in r.json()['data']:
+        for entry in result['data']:
             for i, name in enumerate(names):
                 info[name].append(entry[i])
 
         # Setup intervals
         intervals = []
         for start, end in zip(info['begin_time'], info['end_time']):
-            intervals.append([time.parse_time(start).datetime,
-                              time.parse_time(end).datetime])
+            intervals.append([pd.Timestamp(start).to_pydatetime(),
+                              pd.Timestamp(end).to_pydatetime()])
 
         df = pd.DataFrame(intervals, columns = ['Start', 'End'])
         df.to_csv(self.latest_path, index=False)
