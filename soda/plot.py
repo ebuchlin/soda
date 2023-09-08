7abaeeaad359c8296920ee3b8ed395f8e4eea92b
Eric Buchlin <eric.buchlin@ias.u-psud.fr>
2023-09-08 16:02:12 +0200

    Get list of descriptors automatically

--- soda/plot.py
+++ soda/plot.py
@@ -8,6 +8,7 @@ from bokeh.layouts import gridplot, Spacer, column
 
 from soda.availability import DataProduct
 from soda.trajectory import get_traj
+from soda.soar_query import SoarQuery
 
 
 class DataAvailabilityPlotter:
@@ -26,19 +27,10 @@ class DataAvailabilityPlotter:
         self.plotter.ygrid.grid_line_color = None
         self.plotter.outline_line_color = None
 
-        self.all_options = [
-            # "epd-ept-asun-burst-ele-close", "epd-ept-asun-burst-ion", "epd-ept-asun-hcad", "epd-ept-asun-rates", "epd-ept-north-burst-ele-close", "epd-ept-north-burst-ion", "epd-ept-north-hcad", "epd-ept-north-rates", "epd-ept-south-burst-ele-close", "epd-ept-south-burst-ion", "epd-ept-south-hcad", "epd-ept-south-rates", "epd-ept-sun-burst-ele-close", "epd-ept-sun-burst-ion", "epd-ept-sun-hcad", "epd-ept-sun-rates", "epd-het-asun-burst", "epd-het-asun-rates", "epd-het-north-burst", "epd-het-north-rates", "epd-het-south-burst", "epd-het-south-rates", "epd-het-sun-burst", "epd-het-sun-rates", "epd-sis-a-hehist", "epd-sis-a-rates-fast", "epd-sis-a-rates-medium", "epd-sis-a-rates-slow", "epd-sis-b-hehist", "epd-sis-b-rates-fast", "epd-sis-b-rates-medium", "epd-sis-b-rates-slow", "epd-step-burst", "epd-step-hcad", "epd-step-main", "epd-step-rates",
-            "eui-fsi174-image", "eui-fsi304-image", "eui-hrieuv174-image", "eui-hrieuvnon-image", "eui-hrieuvzer-image", "eui-hrilya1216-image",
-            # "mag-rtn-burst", "mag-rtn-ll", "mag-rtn-ll-1-minute", "mag-rtn-normal", "mag-rtn-normal-1-minute", "mag-srf-burst", "mag-srf-ll", "mag-srf-normal", "mag-vso-burst", "mag-vso-normal", "mag-vso-normal-1-minute",
-            "metis-uv-image", "metis-vl-image", "metis-vl-pb", "metis-vl-pol-angle", "metis-vl-stokes", "metis-vl-tb",
-            "phi-hrt-bazi", "phi-hrt-binc", "phi-hrt-blos", "phi-hrt-bmag", "phi-hrt-icnt", "phi-hrt-stokes", "phi-hrt-vlos",
-            # "rpw-hfr-surv", "rpw-lfr-surv-asm", "rpw-lfr-surv-bp1", "rpw-lfr-surv-bp2", "rpw-lfr-surv-cwf-b", "rpw-lfr-surv-cwf-e", "rpw-lfr-surv-swf-b", "rpw-lfr-surv-swf-e", "rpw-tds-surv-hist1d", "rpw-tds-surv-hist2d", "rpw-tds-surv-mamp", "rpw-tds-surv-rswf-b", "rpw-tds-surv-rswf-e", "rpw-tds-surv-stat", "rpw-tds-surv-tswf-b", "rpw-tds-surv-tswf-e", "rpw-tnr-surv",
-            "solohi-1ft", "solohi-1ut", "solohi-1vt", "solohi-2ft", "solohi-2us", "solohi-2ut", "solohi-3fg", "solohi-3ft", "solohi-3ut", "solohi-4fg", "solohi-4ut",
-            "spice-n-exp", "spice-n-ras", "spice-n-sit", "spice-w-exp", "spice-w-ras", "spice-w-sit",
-            #"swa-eas-pad-def", "swa-eas-pad-dnf", "swa-eas-pad-psd", "swa-eas1-nm3d-def", "swa-eas1-nm3d-dnf", "swa-eas1-nm3d-psd", "swa-eas1-ss-def", "swa-eas1-ss-dnf", "swa-eas1-ss-psd", "swa-eas2-nm3d-def", "swa-eas2-nm3d-dnf", "swa-eas2-nm3d-psd", "swa-eas2-ss-def", "swa-eas2-ss-dnf", "swa-eas2-ss-psd", "swa-pas-eflux", "swa-pas-grnd-mom", "swa-pas-vdf",
-        ][::-1]
+        instruments = ["EUI", "METIS", "PHI", "SOLOHI", "SPICE", "STIX"]
+        soar = SoarQuery()
+        self.all_options = soar.get_descriptors(instruments)[::-1]
 
-        '''
         self.multi_choice = MultiChoice(
             value=self.all_options,
             options=self.all_options,
@@ -46,7 +38,6 @@ class DataAvailabilityPlotter:
             width=200,
             sizing_mode='stretch_height',
             title='Select data products')
-        '''
 
         self.r_plot = figure(sizing_mode='stretch_width', plot_height=150,
                              x_axis_type='datetime', y_range=[0.25, 1.05],
@@ -110,7 +101,8 @@ class DataAvailabilityPlotter:
                 'SPI': '#a65628',
                 'SOL': '#dd1c77',
                 'PHI': '#00cccc',
-                'MET': '#aacc00'
+                'MET': '#aacc00',
+                'STI': '#444444'
                 }[instrument]
 
     @staticmethod
